f2690fa0b833eddac298867a3963ded20a943498
PotatoWhite <bravopotato@gmail.com>
2020-07-22 15:37:42 +0900

    some changed

--- src/main/java/me/potato/demo/demospringboot/sample/SampleRepository.java
+++ src/main/java/me/potato/demo/demospringboot/sample/SampleRepository.java
@@ -3,7 +3,7 @@ package me.potato.demo.demospringboot.sample;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RestResource;
 
-@RestResource
+@RestResource 
 public interface SampleRepository extends PagingAndSortingRepository<Sample, Long> {
     
 }
